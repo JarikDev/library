834f54dbdf0e08c7538784003c76365e3f024c1f
ivan4000 <sxln1@mail.ru>
2019-02-05 20:58:58 +0300

    @Lob it works but not sure its upploading anything

--- src/main/java/com/jarik/library/controllers/BookController.java
+++ src/main/java/com/jarik/library/controllers/BookController.java
@@ -30,96 +30,15 @@ public class BookController {
         return "add-book";
     }
 
-
-
-
-
-
-
-
-
-
-    /*    //old one
-        @PostMapping("/addbook")
-        public String addBook(@Valid Book book, BindingResult result, Model model) {
-            if (result.hasErrors()) {
-                return "add-book";
-            }
-
-            bookRepository.save(book);
-            model.addAttribute("books", bookRepository.findAll());
-            return "index";
-        }*/
-    //new one
-// POST: Do Upload
-    @RequestMapping(value = "/uploadOneFile", method = RequestMethod.POST)
-    public String uploadOneFileHandlerPOST(HttpServletRequest request, //
-                                           Model model, //
-                                           @ModelAttribute("myUploadForm") Book book) {
-
-          this.doUpload(request, model, book);
+    @PostMapping("/addbook")
+    public String addBook(@Valid Book book, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-book";
+        }
         bookRepository.save(book);
         model.addAttribute("books", bookRepository.findAll());
         return "index";
-
     }
-
-    private String doUpload(HttpServletRequest request, Model model, //
-                            Book book) {
-
-        String description = book.getBookDescription();
-        System.out.println("Description: " + description);
-
-        // Root Directory.
-        String uploadRootPath = request.getServletContext().getRealPath("upload");
-        System.out.println("uploadRootPath=" + uploadRootPath);
-
-        File uploadRootDir = new File(uploadRootPath);
-        // Create directory if it not exists.
-        if (!uploadRootDir.exists()) {
-            uploadRootDir.mkdirs();
-        }
-        MultipartFile[] fileDatas = book.getBookData();
-        //
-        List<File> uploadedFiles = new ArrayList<File>();
-        List<String> failedFiles = new ArrayList<String>();
-
-        for (MultipartFile fileData : fileDatas) {
-
-            // Client File Name
-            String name = fileData.getOriginalFilename();
-            System.out.println("Client File Name = " + name);
-
-            if (name != null && name.length() > 0) {
-                try {
-                    // Create the file at server
-                    File serverFile = new File(uploadRootDir.getAbsolutePath() + File.separator + name);
-
-                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-                    stream.write(fileData.getBytes());
-                    stream.close();
-                    //
-                    uploadedFiles.add(serverFile);
-                    System.out.println("Write file: " + serverFile);
-                } catch (Exception e) {
-                    System.out.println("Error Write file: " + name);
-                    failedFiles.add(name);
-                }
-            }
-        }
-        model.addAttribute("description", description);
-        model.addAttribute("uploadedFiles", uploadedFiles);
-        model.addAttribute("failedFiles", failedFiles);
-        return "uploadResult";
-    }
-
-
-
-
-
-
-
-
 
     @GetMapping("/edit/{id}")
     public String showUpdateForm(@PathVariable("id") long id, Model model) {
